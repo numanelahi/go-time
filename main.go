48822a379d8a6e68bfb34e90c694ec3d0069f1b4
Numan Elahi <elahinouman.elahi@gmail.com>
2020-02-06 08:40:29 +0530

    Adds more date time formats to the code

--- main.go
+++ main.go
@@ -14,12 +14,23 @@ func main() {
 	switch *format {
 	case "default":
 		fallthrough
+	case "ymd":
+		year, month, day := time.Now().Date()
+		fmt.Printf("%d-%d-%d\n", year, month, day)
+	case "year", "y":
+		fmt.Printf("%d\n", time.Now().Year())
+	case "month", "m":
+		fmt.Printf("%s\n", time.Now().Month().String())
+	case "day", "d":
+		fmt.Printf("%d\n", time.Now().Day())
+	case "ym":
+		fmt.Printf("%d-%d\n", time.Now().Year(), time.Now().Month())
 	case "utc":
 		fmt.Printf("%s\n", time.Now().UTC())
 	case "unix":
 		fmt.Printf("%d\n", time.Now().Unix())
 	default:
-		fmt.Print("Not a valid time fromat")
+		fmt.Println("Not a valid time fromat")
 		os.Exit(1)
 	}
 	os.Exit(0)
